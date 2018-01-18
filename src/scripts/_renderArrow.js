e9438558aaccd24e5a0705f409585c092d56e549
topus009 <sj_89@mail.ru>
2018-01-19 01:10:22 +0300

    04

--- src/scripts/_renderArrow.js
+++ src/scripts/_renderArrow.js
@@ -1,4 +1,4 @@
-export default function renderArrow(task, index, color) {
+export default function renderArrow(task, index, pos) {
   const div = document.createElement('div');
   div.id = 'curve_' + index;
   div.className = 'curve';
@@ -8,12 +8,20 @@ export default function renderArrow(task, index, color) {
   ds.width = (task * 40) + 'px';
   ds.height = ((task * 40) * 0.5) + 'px';
   ds.borderRadius = (task * 40) + 'px ' + (task * 40) + 'px 0 0';
-  ds.borderColor = color;
   ds.borderWidth = '2px';
   ds.borderBottom = 0;
   ds.top = -((task * 40) / 2) + 'px';
-  ds.left = '-1px';
+  ds.left = -1 + pos + 'px';
+  ds.borderColor = '#E33E91';
 
   document.getElementById('ruler').appendChild(div);
+  // ===============
+  const input = document.createElement('input');
+  input.id = 'input_' + index;
+  input.maxLength = '2';
+  input.className = 'input';
+  div.appendChild(input);
 }
-// ===================
+// =================
+// ==================
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,36 +4,81 @@ import calcTasks from './_calcTasks.js';
 import renderArrow from './_renderArrow';
 
 document.addEventListener('DOMContentLoaded', function () {
-  // ===================
+  // ==================
   (function () {
     const newData = calcTasks(config);
     console.warn(newData);
+    // for () {
+
+    // }
     newData.forEach((e, i) => {
-      if (i === 0) {
-        console.info(i);
-        console.info(e);
-        const color = ['#31ff72', '#00ca3f', '#00910c'];
-        renderArrow(e.a, i, color[0]);
-        renderArrow(e.b, i, color[1]);
-        renderArrow(e.res, i, color[2]);
-      }
-      if (i === 1) {
-        console.info(i);
-        console.info(e);
-        const color = ['#16cfff', '#00a6d1', '#0019d7'];
-        renderArrow(e.a, i, color[0]);
-        renderArrow(e.b, i, color[1]);
-        renderArrow(e.res, i, color[2]);
-      }
-      if (i === 2) {
-        console.info(i);
-        console.info(e);
-        const color = ['#ffc620', '#f29200', '#c65c00'];
-        renderArrow(e.a, i, color[0]);
-        renderArrow(e.b, i, color[1]);
-        renderArrow(e.res, i, color[2]);
-      }
+      // =========== index = 1
+      // if (i === 1) {
+      console.info(i);
+      console.info(e);
+      const pos = e.a * 38;
+      (function step_1() {
+        const block_a = document.getElementById('a');
+        const block_b = document.getElementById('b');
+
+        block_a.innerHTML = e.a;
+        block_b.innerHTML = e.b;
+        renderArrow(e.a, i, 0);
+        const arrow_input_1 = document.getElementById('input_' + i);
+
+        arrow_input_1.addEventListener('change', function (x1) {
+          if (+x1.target.value !== e.a) {
+            arrow_input_1.style.color = 'red';
+            block_a.style.backgroundColor = '#FBA846';
+          }
+          if (+x1.target.value === e.a) {
+            arrow_input_1.style.color = 'black';
+            block_a.style.backgroundColor = 'transparent';
+            const step_2 = function () {
+              renderArrow(e.b, i + 1, pos);
+              const arrow_input_2 = document.getElementById('input_' + (i + 1));
+              arrow_input_2.addEventListener('change', function (x2) {
+                if (+x2.target.value !== e.b) {
+                  arrow_input_2.style.color = 'red';
+                  block_b.style.backgroundColor = '#FBA846';
+                }
+                if (+x2.target.value === e.b) {
+                  arrow_input_2.style.color = 'black';
+                  block_b.style.backgroundColor = 'transparent';
+                  const res_input = document.getElementById('res_input');
+                  res_input.classList.remove('hidden');
+                  res_input.addEventListener('change', function (res) {
+                    if (+res.target.value !== e.res) {
+                      res_input.style.color = 'red';
+                    }
+                    if (+res.target.value === e.res) {
+                      res_input.style.color = 'black';
+                      res_input.classList.add('hidden');
+                      document.getElementById('res').innerText = e.res;
+                    }
+                  });
+                }
+              });
+            };
+            step_2();
+          }
+        });
+      }());
+      // }
+      // ================ index = 1
     });
   }());
-  // ==================
+  // ================
 });
+/*
+*
+
+*
+
+*
+
+// renderArrow(e.res, i, 0);
+
+*
+
+*/
