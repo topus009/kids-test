c8adf56f7ac431213552e1fd3f4e969b82372b55
topus009 <sj_89@mail.ru>
2018-01-19 04:59:08 +0300

    06

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,24 +6,33 @@ import step_1 from './_step_1';
 
 document.addEventListener('DOMContentLoaded', function () {
   // ==================
-  (function () {
+  let COUNT = 0;
+
+  (function main() {
+    let RESULT = null;
     const newData = calcTasks(config);
+    const e = newData[COUNT];
+    const pos = e.a * 38;
     const block_a = document.getElementById('a');
     const block_b = document.getElementById('b');
+
     console.warn(newData);
 
-    for (let i = 0; i < newData.length; i++) {
-      const e = newData[i];
-      const pos = e.a * 38;
-      const RESULT = false;
+    block_a.innerHTML = e.a;
+    block_b.innerHTML = e.b;
 
-      block_a.innerHTML = e.a;
-      block_b.innerHTML = e.b;
-
-      step_1(e, i, renderArrow, block_a, block_b, pos);
-      if (!RESULT) {
-        break;
-      }
-    }
+    step_1(e, COUNT, renderArrow, block_a, block_b, pos);
   }());
 });
+
+
+// COUNT += 1;
+// const check_result = function () {
+//   const res = document.getElementById('res').innerText;
+//   console.log(`%c ${res}`, 'color: #19FF00;');
+//   RESULT = res;
+//   while (RESULT === e.res && COUNT < newData.length) {
+//     main();
+//   }
+// };
+// setInterval(check_result, 2000);
